6bb37ec01d9869cd3d024f8d5196d924ae512c20
GitHub Action <action@github.com>
2021-07-10 08:31:03 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/senid231/jetbrainsprecommitscriptplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.senid231.jetbrainsprecommitscriptplugin.services
+
+import com.github.senid231.jetbrainsprecommitscriptplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/senid231/jetbrainsprecommitscriptplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/senid231/jetbrainsprecommitscriptplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.senid231.jetbrainsprecommitscriptplugin.services
 
+import com.github.senid231.jetbrainsprecommitscriptplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
